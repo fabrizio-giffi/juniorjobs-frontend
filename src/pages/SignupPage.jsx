663c94fa36d396846d19ebc6048a8506540d0847
Draian123 <beiteldennis3@yahoo.de>
2023-03-02 16:07:35 +0100

    addded useContext

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Box, Button, TextField } from "@mui/material";
 import axios from "axios";
