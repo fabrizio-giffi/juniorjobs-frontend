0d9db5e454cca85ade0b32440afd835daabacce6
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-03-03 16:08:45 +0100

    working on profile page

--- src/components/CompanyProfilePublic.jsx
+++ src/components/CompanyProfilePublic.jsx
@@ -2,20 +2,16 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 import './CompanyProfile.css';
-import { Card } from "@mui/material";
 
 const API_URL = "http://localhost:5005/api/company";
 
 function CompanyProfile() {
   const { user } = useContext(AuthContext);
   const [profile, setProfile] = useState();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState(null);
-  const [street, setStreet] = useState('');
-  const [zipCode, setZipCode] = useState('');
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
 
   const getProfile = async () => {
     try {
@@ -45,15 +41,11 @@ function CompanyProfile() {
     getProfile();
   }, []);
 
-console.log(profile)
-
   return (
     profile && (
       <>
-        <div className="title">This is the company profile</div>
-        <form onSubmit={handleEdit} className="edit-form">
-          <h2>Company information</h2>
-          <h6>Click on the information to edit</h6>
+        <div>This is the company profile</div>
+        <form onSubmit={handleEdit}>
           <span>
             Company name:
             <input
@@ -74,51 +66,6 @@ console.log(profile)
               value={email}
             />
           </span>
-          <span>Address: </span>
-          <div className="information">
-            <span>
-            Street:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>
-          Zip Code:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>
-            City:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>
-            Country:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          </div>
-          
-         
 
           {message && <span>{message}</span>}
           <button type="submit">edit information</button>
@@ -136,15 +83,14 @@ console.log(profile)
               All your job posts
             </h4>
             <ul className="ul-jobposts">
+            {console.log(profile)}
               {profile.jobPosts.map((jobPost) => {
-                const date = new Date(jobPost.createdAt).toLocaleDateString()
+                console.log("SINGLE JOB POST", jobPost)
                 return (
-                  <Card key={jobPost._id}>
+                  <div key={jobPost._id}>
                     <li>{jobPost.title}</li>
-                    <li>{jobPost.description.jobtype}</li>
-                    <li>{jobPost.salaryRange.minimum} - {jobPost.salaryRange.maximum}</li>
-                    <span>Posted on:{date}</span>
-                  </Card>
+
+                  </div>
                 );
               })}
             </ul>
