2af0db8cdf81e47d0731b32c98861ea80922efed
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-03-03 17:55:43 +0100

    profile pages

--- src/components/CompanyProfile.jsx
+++ src/components/CompanyProfile.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
-import './CompanyProfile.css';
+import "./CompanyProfile.css";
 import { Card } from "@mui/material";
 
 const API_URL = "http://localhost:5005/api/company";
@@ -12,10 +12,11 @@ function CompanyProfile() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState(null);
-  const [street, setStreet] = useState('');
-  const [zipCode, setZipCode] = useState('');
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
+  const [street, setStreet] = useState("Please fill in your street");
+  const [zipCode, setZipCode] = useState("Please fill in your zip code");
+  const [city, setCity] = useState("Please fill in your city");
+  const [country, setCountry] = useState("Please fill in your country");
+  const [profilePicture, setProfilePicture] = useState("");
 
   const getProfile = async () => {
     try {
@@ -23,6 +24,11 @@ function CompanyProfile() {
       setProfile(response.data);
       setName(response.data.name);
       setEmail(response.data.email);
+      setStreet(response.data.address.street);
+      setZipCode(response.data.address.zipCode);
+      setCity(response.data.address.city);
+      setCountry(response.data.address.country);
+      setProfilePicture(response.data.profilePicture);
     } catch (error) {
       console.log("There was an error getting the profile", error);
     }
@@ -30,7 +36,15 @@ function CompanyProfile() {
 
   const handleEdit = async (event) => {
     event.preventDefault();
-    const requestBody = { name, email };
+    const requestBody = {
+      name,
+      email,
+      street,
+      zipCode,
+      city,
+      country,
+      profilePicture,
+    };
     try {
       const response = await axios.put(
         `${API_URL}/edit/${user.id}`,
@@ -45,110 +59,124 @@ function CompanyProfile() {
     getProfile();
   }, []);
 
-console.log(profile)
-
   return (
     profile && (
       <>
-        <div className="title">This is the company profile</div>
+        <div> This is the company profile</div>
         <form onSubmit={handleEdit} className="edit-form">
-          <h2>Company information</h2>
-          <h6>Click on the information to edit</h6>
-          <span>
-            Company name:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={name}
-              onChange={(event) => setName(event.target.value)}
-              value={name}
+          <div className="title">
+            <img
+              src={`https://api.dicebear.com/5.x/initials/svg?seed=${name}`}
+              alt={name}
             />
-          </span>
-          <span>
-            Company email:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>Address: </span>
-          <div className="information">
-            <span>
-            Street:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>
-          Zip Code:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>
-            City:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
-          <span>
-            Country:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
+            <h2>Company information</h2>
           </div>
-          
-         
+          <h6>Click on the information to edit</h6>
+          <div className="information">
+            <div className="input-label">
+              <label>Company name:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={name}
+                onChange={(event) => setName(event.target.value)}
+                value={name}
+              />
+            </div>
+            <div className="input-label">
+              <label>Company email:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={email}
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+              />
+            </div>
+
+            <span className="address">Address: </span>
+            <div className="input-label">
+              <label>Street:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={street}
+                onChange={(event) => setStreet(event.target.value)}
+                value={street}
+              />
+            </div>
+            <div className="input-label">
+              <label>Zip Code:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={zipCode}
+                onChange={(event) => setZipCode(event.target.value)}
+                value={zipCode}
+              />
+            </div>
+            <div className="input-label">
+              <label>City:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={city}
+                onChange={(event) => setCity(event.target.value)}
+                value={city}
+              />
+            </div>
+            <div className="input-label">
+              <label>Country:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={country}
+                onChange={(event) => setCountry(event.target.value)}
+                value={country}
+              />
+            </div>
+            <div className="input-label">
+              <label>profile picture:</label>
+              <input
+                style={{ border: "none", outline: "none" }}
+                type="text"
+                placeholder={profilePicture}
+                onChange={(event) => setProfilePicture(event.target.value)}
+                value={profilePicture}
+              />
+            </div>
+          </div>
 
           {message && <span>{message}</span>}
           <button type="submit">edit information</button>
         </form>
 
         <div className="favorites">
-            <ul>
-              {profile.favorites.map((favorite) => {
-                <li>{favorite}</li>;
-              })}
-            </ul>
-          </div>
-          <div className="jobPosts">
-            <h4>
-              All your job posts
-            </h4>
-            <ul className="ul-jobposts">
-              {profile.jobPosts.map((jobPost) => {
-                const date = new Date(jobPost.createdAt).toLocaleDateString()
-                return (
-                  <Card key={jobPost._id}>
-                    <li>{jobPost.title}</li>
-                    <li>{jobPost.description.jobtype}</li>
-                    <li>{jobPost.salaryRange.minimum} - {jobPost.salaryRange.maximum}</li>
-                    <span>Posted on:{date}</span>
-                  </Card>
-                );
-              })}
-            </ul>
-          </div>
+          <ul>
+            {profile.favorites.map((favorite) => {
+              <li>{favorite}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="jobPosts">
+          <h4>All your job posts</h4>
+          <ul className="ul-jobposts">
+            {profile.jobPosts.map((jobPost) => {
+              const date = new Date(jobPost.createdAt).toLocaleDateString();
+              return (
+                <Card key={jobPost._id}>
+                  <li>{jobPost.title}</li>
+                  <li>{jobPost.description.jobtype}</li>
+                  <li>
+                    {jobPost.salaryRange.minimum} -{" "}
+                    {jobPost.salaryRange.maximum}
+                  </li>
+                  <span>Posted on:{date}</span>
+                </Card>
+              );
+            })}
+          </ul>
+        </div>
       </>
     )
   );
--- src/components/CompanyProfilePublic.jsx
+++ src/components/CompanyProfilePublic.jsx
@@ -1,42 +1,42 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
-import './CompanyProfile.css';
+import "./CompanyProfile.css";
+import { Card } from "@mui/material";
+import { useParams } from "react-router-dom";
 
 const API_URL = "http://localhost:5005/api/company";
 
-function CompanyProfile() {
+function CompanyProfilePublic() {
   const { user } = useContext(AuthContext);
   const [profile, setProfile] = useState();
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState(null);
-
+  const [street, setStreet] = useState("Please fill in your street");
+  const [zipCode, setZipCode] = useState("Please fill in your zip code");
+  const [city, setCity] = useState("Please fill in your city");
+  const [country, setCountry] = useState("Please fill in your country");
+  const [profilePicture, setProfilePicture] = useState("");
+  const params = useParams();
+  const { id } = params;
+  console.log("IDDDD", id);
   const getProfile = async () => {
     try {
-      const response = await axios.get(`${API_URL}/${user.id}`);
+      const response = await axios.get(`${API_URL}/${id}`);
       setProfile(response.data);
       setName(response.data.name);
       setEmail(response.data.email);
+      setStreet(response.data.address.street);
+      setZipCode(response.data.address.zipCode);
+      setCity(response.data.address.city);
+      setCountry(response.data.address.country);
+      setProfilePicture(response.data.profilePicture);
     } catch (error) {
       console.log("There was an error getting the profile", error);
     }
   };
 
-  const handleEdit = async (event) => {
-    event.preventDefault();
-    const requestBody = { name, email };
-    try {
-      const response = await axios.put(
-        `${API_URL}/edit/${user.id}`,
-        requestBody
-      );
-      setMessage(response.data.message);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
     getProfile();
   }, []);
@@ -44,60 +44,78 @@ function CompanyProfile() {
   return (
     profile && (
       <>
-        <div>This is the company profile</div>
-        <form onSubmit={handleEdit}>
-          <span>
-            Company name:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={name}
-              onChange={(event) => setName(event.target.value)}
-              value={name}
-            />
-          </span>
-          <span>
-            Company email:
-            <input
-              style={{ border: "none", outline: "none" }}
-              type="text"
-              placeholder={email}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </span>
+        <div> This is the company profile</div>
 
-          {message && <span>{message}</span>}
-          <button type="submit">edit information</button>
-        </form>
+        <div className="title">
+          <img
+            src={`https://api.dicebear.com/5.x/initials/svg?seed=${name}`}
+            alt={name}
+          />
+          <h2>Company information</h2>
+        </div>
+        <h6>Click on the information to edit</h6>
+        <div className="information">
+          <div className="input-label">
+            <label>Company name:</label>
+            <span>{name}</span>
+          </div>
+          <div className="input-label">
+            <label>Company email:</label>
+            <span>{email}</span>
+          </div>
+
+          <span className="address">Address: </span>
+          <div className="input-label">
+            <label>Street:</label>
+            <span>{street}</span>
+          </div>
+          <div className="input-label">
+            <label>Zip Code:</label>
+            <span>{zipCode}</span>
+          </div>
+          <div className="input-label">
+            <label>City:</label>
+            <span>{city}</span>
+          </div>
+          <div className="input-label">
+            <label>Country:</label>
+            <span>{country}</span>
+          </div>
+          <div className="input-label">
+            <label>profile picture:</label>
+            <span>{profilePicture}</span>
+          </div>
+        </div>
 
         <div className="favorites">
-            <ul>
-              {profile.favorites.map((favorite) => {
-                <li>{favorite}</li>;
-              })}
-            </ul>
-          </div>
-          <div className="jobPosts">
-            <h4>
-              All your job posts
-            </h4>
-            <ul className="ul-jobposts">
-            {console.log(profile)}
-              {profile.jobPosts.map((jobPost) => {
-                console.log("SINGLE JOB POST", jobPost)
-                return (
-                  <div key={jobPost._id}>
-                    <li>{jobPost.title}</li>
-
-                  </div>
-                );
-              })}
-            </ul>
-          </div>
+          <ul>
+            {profile.favorites.map((favorite) => {
+              <li>{favorite}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="jobPosts">
+          <h4>All your job posts</h4>
+          <ul className="ul-jobposts">
+            {profile.jobPosts.map((jobPost) => {
+              const date = new Date(jobPost.createdAt).toLocaleDateString();
+              return (
+                <Card key={jobPost._id}>
+                  <li>{jobPost.title}</li>
+                  <li>{jobPost.description.jobtype}</li>
+                  <li>
+                    {jobPost.salaryRange.minimum} -{" "}
+                    {jobPost.salaryRange.maximum}
+                  </li>
+                  <span>Posted on:{date}</span>
+                </Card>
+              );
+            })}
+          </ul>
+        </div>
       </>
     )
   );
 }
 
-export default CompanyProfile;
+export default CompanyProfilePublic;
