458521c7d4e99ad2ad4b61a2b9335e4847523e36
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-03-08 17:54:42 +0100

    css profile page and input file component

---
+++ src/components/UploadFileBtn.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@mui/material";
+import React, { useRef, useState } from "react";
+import "./CloudinaryUploadWidget";
+// Style the Button component
+
+const UploadFileBtn = () => {
+  const [fileName, setFileName] = useState("");
+  const hiddenFileInput = useRef(null);
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0].name;
+    setFileName(fileUploaded);
+  };
+
+  return (
+    <>
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ bgcolor: "#6b9080", mt: 3, mb: 2 }}
+        onClick={handleClick}
+      >
+        Upload a file
+      </Button>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        style={{ display: "none" }}
+      />
+      <span>{fileName}</span>
+    </>
+  );
+};
+export default UploadFileBtn;
