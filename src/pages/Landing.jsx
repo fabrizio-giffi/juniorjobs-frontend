d76c5e103dfb0489fd190270ed734d41a007740c
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-03-09 17:34:42 +0100

    Fix link in the landing page. Minor cleanup in landing page.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,64 +1,36 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import {Link} from 'react-router-dom'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import './Landing.css'
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-
-const theme = createTheme();
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import "./Landing.css";
 
 export default function Landing() {
   return (
-    <div className='flexcontainer'>
-        <div className='collumn headline-box'>
-            <Typography
-              component="h1"
-              variant="h2"
-              align=''
-              color="text.primary"
-              gutterBottom
-            >
-              Junior Jobs
-            </Typography>
-            <Typography variant="h5" alignContent={{ md: "start", xs: 'center'}} color="text.secondary" paragraph>
-              The platform that connects employees and employers for junior level positions.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent={{ md: "start", xs: 'center'}}
-            >
-            <div style={{width: "60%"}}>
-            <Link to={"http://localhost:5173/jobs"}>
-              <Button variant="contained" fullWidth sx={{ bgcolor: "#6b9080" }} size="large">Try It Out</Button>
+    <div className="flexcontainer">
+      <div className="collumn headline-box">
+        <Typography component="h1" variant="h2" align="" color="text.primary" gutterBottom>
+          Junior Jobs
+        </Typography>
+        <Typography variant="h5" alignContent={{ md: "start", xs: "center" }} color="text.secondary" paragraph>
+          The platform that connects employees and employers for junior level positions.
+        </Typography>
+        <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent={{ md: "start", xs: "center" }}>
+          <div style={{ width: "60%" }}>
+            <Link to="/jobs">
+              <Button variant="contained" fullWidth sx={{ bgcolor: "#6b9080" }} size="large">
+                Try It Out
+              </Button>
             </Link>
-            </div>
-              </Stack>
+          </div>
+        </Stack>
+      </div>
+      <div className="collumn">
+        <div className="triangle"></div>
+        <div className="background-image displaynone">
+          <img src="landing.jpg" alt="Resume" />
         </div>
-        <div className='collumn'>
-            <div className='triangle'></div>
-            <div className='background-image displaynone'>
-              <img src='landing.jpg' alt="Resume" />
-            </div>
-        </div>
-
-
+      </div>
     </div>
   );
 }
