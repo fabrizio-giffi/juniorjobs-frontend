bbb6f47b318c5f1023fb44be33cb4bd4c33f3431
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-03-25 23:00:52 +0100

    logo and mobile navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,54 +1,131 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import "./NavBar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { Button } from "@mui/material";
 
 function NavBar() {
   const { isLoggedIn, logOutUser, user } = useContext(AuthContext);
+  const [cross, setCross] = useState(false);
 
   return (
-    <ul className="ul-navbar">
-      {!isLoggedIn && (
-        <>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/jobs">Jobs</NavLink>
-          </li>
-          <li>
-            <NavLink to="/signup">Signup</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-        </>
-      )}
-      {isLoggedIn && (
-        <>
-          <li>
-            <NavLink to={user.role === "company" ? "/junior" : "/jobs"}>
-              {user.role === "company" ? "Juniors" : "Jobs"}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
-          </li>
-          {user.role === "company" && (
+    <>
+      <div className="ul-navbar">
+        <NavLink to='/' >
+          <img src="../juniorjobs.png" alt="logo junior jobs" />
+        </NavLink>
+
+        <ul className="ul-desktop">
+          {!isLoggedIn && (
+            <>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/jobs">Jobs</NavLink>
+              </li>
+              <li>
+                <NavLink to="/signup">Signup</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <li>
+                <NavLink to={user.role === "company" ? "/junior" : "/jobs"}>
+                  {user.role === "company" ? "Juniors" : "Jobs"}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/profile">Profile</NavLink>
+              </li>
+              {user.role === "company" && (
+                <li>
+                  <NavLink to="/create-post">Create job post</NavLink>
+                </li>
+              )}
+            </>
+          )}
+          {isLoggedIn && (
             <li>
-              <NavLink to="/create-post">Create job post</NavLink>
+              <NavLink to="/" onClick={logOutUser}>
+                Logout
+              </NavLink>
             </li>
           )}
-        </>
+        </ul>
+
+        <Button className="burger">
+          {!cross ? (
+            <MenuIcon sx={{ color: "grey" }} onClick={(e) => setCross(true)} />
+          ) : (
+            <CloseIcon
+              sx={{ color: "grey" }}
+              onClick={(e) => setCross(false)}
+            />
+          )}
+        </Button>
+      </div>
+
+      {cross ? (
+        <div className="dropdown">
+          <ul className="ul-mobile">
+            {!isLoggedIn && (
+              <>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <hr />
+                <li>
+                  <NavLink to="/jobs">Jobs</NavLink>
+                </li>
+                <hr />
+                <li>
+                  <NavLink to="/signup">Signup</NavLink>
+                </li>
+                <hr />
+                <li>
+                  <NavLink to="/login">Login</NavLink>
+                </li>
+              </>
+            )}
+            {isLoggedIn && (
+              <>
+                <li>
+                  <NavLink to={user.role === "company" ? "/junior" : "/jobs"}>
+                    {user.role === "company" ? "Juniors" : "Jobs"}
+                  </NavLink>
+                </li>
+                <hr />
+                <li>
+                  <NavLink to="/profile">Profile</NavLink>
+                </li>
+                <hr />
+                {user.role === "company" && (
+                  <li>
+                    <NavLink to="/create-post">Create job post</NavLink>
+                  </li>
+                )}
+              </>
+            )}
+            {isLoggedIn && (
+              <li>
+                <NavLink to="/" onClick={logOutUser}>
+                  Logout
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
+      ) : (
+        ""
       )}
-      {isLoggedIn && (
-        <li>
-          <NavLink to="/" onClick={logOutUser}>
-            Logout
-          </NavLink>
-        </li>
-      )}
-    </ul>
+    </>
   );
 }
 
