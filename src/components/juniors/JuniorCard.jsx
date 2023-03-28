6c066b0ac91e84a3af4ea71c27491007398d2e2b
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-03-28 15:08:06 +0200

    Final touches to Junior List page

--- src/components/juniors/JuniorCard.jsx
+++ src/components/juniors/JuniorCard.jsx
@@ -8,6 +8,9 @@ import {
   Button,
   Card,
   Chip,
+  Container,
+  Divider,
+  IconButton,
   List,
   Modal,
   Stack,
@@ -15,7 +18,8 @@ import {
   Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
-import { Container, style } from "@mui/system";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import PlaceIcon from "@mui/icons-material/Place";
 
 const api_INDEX = import.meta.env.VITE_INDEX_URL;
 
@@ -59,17 +63,21 @@ function JuniorCard({ focus }) {
     <Card sx={{ width: "100%", bgcolor: "#fbfbfb", p: 4, m: 4 }}>
       <Link to={`/junior/${focus._id}`}>
         <Avatar sx={{ width: 128, height: 128 }} src={focus.profilePic} />
+        <Divider sx={{ my: 2 }} />
         <Typography variant="h4">
           {focus.firstName} {focus.lastName}
         </Typography>
-        {focus.location && (
-          <Typography variant="h6">
-            {focus.location.country}, {focus.location.city}
-          </Typography>
-        )}
         <Typography variant="body1">{focus.field} junior developer</Typography>
+        {focus.location && (
+          <Stack direction="row" spacing={1} sx={{ display: "flex", alignItems: "center", my: 1 }}>
+            <PlaceIcon color="action" />
+            <Typography variant="body2" color="text.secondary">
+            {focus.location.city}, {focus.location.country}
+            </Typography>
+          </Stack>
+        )}
       </Link>
-      <List sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+      <List sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: 1 }}>
         {focus.skills.length > 0 &&
           focus.skills.map((skill) => {
             return (
@@ -82,13 +90,19 @@ function JuniorCard({ focus }) {
                   textTransform: "none",
                 }}
                 variant="contained"
-                onClick={() => handleClick(skill)}
               />
             );
           })}
       </List>
-      <div>
-        {focus.calendly ? <Button onClick={handleOpen}>Calendly</Button> : ""}
+      <Divider sx={{ my: 2 }} />
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        {focus.calendly ? (
+          <Button variant="contained" sx={{ bgcolor: "#6b9080", mr: 2, display: formShow ? "none" : "block" }} onClick={handleOpen}>
+            Calendly
+          </Button>
+        ) : (
+          ""
+        )}
         <Modal
           sx={{
             maxWidth: "75%",
@@ -96,32 +110,27 @@ function JuniorCard({ focus }) {
             alignItems: "center",
             margin: "auto",
           }}
-          aria-labelledby="edit-info-form"
-          aria-describedby="edit-info-form"
+          aria-labelledby="calendly"
+          aria-describedby="calendly"
           open={open}
           onClose={() => setCalendly(false)}
         >
-          <Container sx={{ bgcolor: "#eaf4f4", padding: 2, width: "100%" }}>
+          <Container sx={{ bgcolor: "#eaf4f4", padding: 2, width: "80%" }}>
+            <IconButton onClick={handleClose}>
+              <ArrowBackIcon />
+            </IconButton>
             <InlineWidget url={focus.calendly} />
-            <Button
-              variant="contained"
-              sx={{ bgcolor: "#6b9080", mt: 3, mb: 2 }}
-              type="button"
-              onClick={handleClose}
-            >
-              Cancel
-            </Button>
           </Container>
         </Modal>
         {messageSent ? (
-          <Typography>
-            An email has been sent to the user. Thanks for using Junior Jobs!
-          </Typography>
+          <Typography>An email has been sent to the user. Thanks for using Junior Jobs!</Typography>
         ) : !formShow ? (
-          <Button onClick={() => setFormShow(true)}>Message</Button>
+          <Button variant="contained" sx={{ bgcolor: "#6b9080" }} onClick={() => setFormShow(true)}>
+            Message
+          </Button>
         ) : (
           <>
-            <Stack spacing={1} component="form" onSubmit={handleMessage}>
+            <Stack sx={{ width: "100%" }} spacing={1} component="form" onSubmit={handleMessage}>
               <TextField
                 type="text"
                 value={subject}
@@ -154,19 +163,23 @@ function JuniorCard({ focus }) {
                 fullWidth
                 onChange={(event) => setContactInfo(event.target.value)}
               />
-              <Stack
-                direction="row"
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                <Button type="submit">Send message</Button>
-                <Button type="submit" onClick={() => setFormShow(false)}>
+              <Stack direction="row" spacing={2} sx={{ display: "flex", justifyContent: "center" }}>
+                <Button variant="contained" sx={{ bgcolor: "#6b9080" }} type="submit">
+                  Send message
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{ bgcolor: "#6b9080" }}
+                  type="submit"
+                  onClick={() => setFormShow(false)}
+                >
                   Cancel
                 </Button>
               </Stack>
             </Stack>
           </>
         )}
-      </div>
+      </Box>
     </Card>
   );
 }
